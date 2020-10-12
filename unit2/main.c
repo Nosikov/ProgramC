ec544b6ad06ea6f3d14f904c8cf932c00f1694e3
Nosikov <nosevg@yandex.ru>
2020-10-12 07:24:05 +0700

    second lesson

---
+++ unit2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int t;
+   t=15;
+   t=(t-2)*t+5;
+   printf("%d",t);
+
+    printf("\n");
+
+    float v;
+    v=2.54;
+    printf("%f",v);
+
+printf("\n");
+ printf("%g",v);
+printf("\n");
+
+ v=t;
+ printf("%f",v);
+    return 0;
+}
